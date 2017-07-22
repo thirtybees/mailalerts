a0ccd2eb3e0d44ac818b000165fe8ee699f2995d
Denver Prophit Jr <denver@denverprophit.us>
2017-07-22 09:39:26 -0400

    Modify trademark/copyright header

--- mailalerts.php
+++ mailalerts.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * 2007-2016 PrestaShop
+ * 2017 - thirty bees
  *
  * NOTICE OF LICENSE
  *
@@ -10,7 +11,7 @@
  * http://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
+ * to info@thirtybees.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
@@ -19,9 +20,12 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
+ * @author	thirty bees <info@thirtybees.com>
  * @copyright 2007-2016 PrestaShop SA
+ * @copyright 2017 - thirty bees
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
+ * U.S.A. Trademark of thirty bees
  */
 
 use MailAlertModule\MailAlert;
