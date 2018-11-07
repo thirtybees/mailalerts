25fade2fbc8a792e513e5ec96a70e5c196a9a337
Mauro Tschiedel <mauroagr@users.noreply.github.com>
2018-11-07 17:32:36 -0200

    Update MailAlert.php

--- classes/MailAlert.php
+++ classes/MailAlert.php
@@ -240,7 +240,7 @@ class MailAlert extends \ObjectModel
             $product = new \Product((int) $idProduct, false, $idLang, $idShop);
             $productLink = $link->getProductLink($product, $product->link_rewrite, null, null, $idLang, $idShop);
             $imageCover = \Product::getCover((int)$idProduct);
-            $imageLinkCover = $link->getImageLink($product->link_rewrite, $cover["id_image"], 'home_default');
+            $imageLinkCover = $link->getImageLink($product->link_rewrite, $imageCover["id_image"], 'home_default');
             $templateVars = [
                 '{product}'      => (is_array($product->name) ? $product->name[$idLang] : $product->name),
                 '{product_link}' => $productLink,
