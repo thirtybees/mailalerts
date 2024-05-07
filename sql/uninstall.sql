b1cbaf008cebfc4d4d0509d46a160da393536976
therampagerado <office@rampagesport.eu>
2024-05-07 17:30:23 +0300

    Update years and use HTTPS for links (where needed) in license headers.
    Update years and use HTTPS for links (where needed) in license headers.

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022 - 2022 thirty bees
+ * Copyright (C) 2017-2024 thirty bees
  *
  * NOTICE OF LICENSE
  *
@@ -12,7 +12,7 @@
  * to license@thirtybees.com so we can send you a copy immediately.
  *
  * @author    thirty bees <modules@thirtybees.com>
- * @copyright 2021 - 2021 thirty bees
+ * @copyright 2017-2024 thirty bees
  * @license   Academic Free License (AFL 3.0)
  */
 DROP TABLE IF EXISTS `PREFIX_mailalert_customer_oos`;
